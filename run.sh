984aaf210e52a211b11651d70951ed8fee4381da
Caleb646 <calebthomas646@yahoo.com>
2024-04-04 00:15:19 -0400

    added run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,5 @@ echo "Root Dir: $ROOT_DIR"
 export DATA_INFO_DIR="$ROOT_DIR/data_info/"
 export DATA_DIR="$ROOT_DIR/view_and_diagnosis_labeled_set/"
 export CHECKPOINT_DIR="$ROOT_DIR/model_checkpoints/"
+
+bash runs/SAMIL/launch_experiment.sh run_here
